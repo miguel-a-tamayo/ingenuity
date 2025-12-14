95ffc5a7b8bf513ff64ac7eb5050c61c65396d4c
Miguel Tamayo <miiguell098@gmail.com>
2025-12-13 18:44:44 -0800

    finished testing the rk4 integrator

--- unitTests/test_integration.py
+++ unitTests/test_integration.py
@@ -5,10 +5,92 @@ test_integration.py
 Test the Runge-Kutta RK4 Integrator
 """
 
+from utilities.integrator import integrateRk4
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 def test_Rk4FirstOrder():
     # test a first order system like an RC cirtuit
-    # system: dV = -(1/RC) * V
+    # system: dV = -(1.0/RC) * V
     # analytical solution: V = V0 * e^(-t/RC)
-    pass
+    v0 = 5.0
+    rc = 10e-1
+
+    t = np.arange(0.0, 5.0, 0.05)
+    v_analytical = v0 * np.exp(-t / rc)
+
+    # rk4 solution
+    dV = lambda t, V, RC: - (1.0 / RC) * V
+    v_rk4 = np.zeros(shape=len(t))
+    v_rk4[0] = 5.0
+    dt = 0.05
+    
+    for k in range(0, len(v_rk4)-1):
+        state = integrateRk4(dV, v_rk4[k], t[k], dt, rc)
+        v_rk4[k+1] = state
+
+    plt.figure(1)
+    plt.plot(t, v_analytical, label="Analytical Solution")
+    plt.plot(t, v_rk4, linestyle='--')
+    plt.grid()
+    plt.title("First Order (RC Circuit) System Integration Test")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Voltage [V]")
+    plt.legend(["Analytical Solution", "RK4 Solution"])
+
+def test_Rk4SecondOrder():
+    # test a second order system like a mass spring damper
+    # m*xdd + c*xd + k*x = 0
+    # analytical solution: x = x0*e^(-zeta*wn*t)cos(wd*t) + (x0*zeta*wn/wd)*e^(-zeta*wn*t)sin(wd*t)
+    # wn = sqrt(k/m), zeta = c / 2sqrt(m*k)
+
+    def derivative(t, state, k, c, m) -> np.array:
+        xd = state[1]
+        xdd = -(c * state[1] + k * state[0]) / m
+
+        return np.array([xd, xdd])
+
+    x0 = 1.0 # [m]
+    zeta = 0.07
+    wn = 2.0 * np.pi * 1.0
+    m = 1.0
+    wd = wn * np.sqrt(1 - np.square(zeta))
+    t = np.arange(0.0, 7.0, 0.01)
+
+    # analytical solution
+    x_analytical = x0 * np.exp(-zeta*wn*t)*np.cos(wd*t) + (x0*zeta*wn/wd)*np.exp(-zeta*wn*t)*np.sin(wd*t)
+    xd1 = x0*np.exp(-zeta*wn*t)*(-zeta*wn*np.cos(wd*t) - wd*np.sin(wd*t))
+    xd2 = (x0*zeta*wn/wd)*np.exp(-zeta*wn*t)*(-zeta*wn*np.sin(wd*t) + wd*np.cos(wd*t))
+    xd_analytical = xd1 + xd2
+
+    # rk4 solution
+    k = m * np.square(wn)
+    c = 2 * zeta * m * wn
+    x_rk4 = np.zeros(len(t))
+    x_rk4[0] = 1.0
+    xd_rk4 = np.zeros(len(t))
+    dt = 0.01
+
+    for i in range(0, len(x_rk4)-1):
+        state = integrateRk4(derivative, np.array([x_rk4[i], xd_rk4[i]]), t[i], dt, k, c, m)
+        x_rk4[i+1] = state[0]
+        xd_rk4[i+1] = state[1]
+
+    plt.figure(2)
+    plt.title("Second Order (Mass-Spring-Damper) System Integration Test")
+    plt.subplot(2, 1, 1)
+    plt.plot(t, x_analytical)
+    plt.plot(t, x_rk4, linestyle='--')
+    plt.grid()
+    plt.ylabel("Position [m]")
+    plt.legend(["Analytical Solution", "RK4 Solution"])
+
+    plt.subplot(2, 1, 2)
+    plt.plot(t, xd_analytical)
+    plt.plot(t, xd_rk4, linestyle='--')
+    plt.grid()
+    plt.xlabel("Time [s]")
+    plt.ylabel("Velocity [m/s]")
+    plt.legend(["Analytical Solution", "RK4 Solution"])
+    plt.show()
--- utilities/integrator.py
+++ utilities/integrator.py
@@ -7,11 +7,11 @@ Defines an RK4 integrator that is used for various states in the simulation
 
 import numpy as np
 
-def integrateRk4(derivative: callable, state: np.array, t: float, dt: float, *args) -> np.array:
+def integrateRk4(derivative: callable, state: np.array, t: float, dt: float, *args):
     """
     integrateRk4(derivative: callable, state: np.array, t: float, dt: float, *args)
 
-    Integrates a statte using Runge-Kutta "RK4" method
+    Integrates a state using Runge-Kutta "RK4" method
 
     :param derivative: callable function that calculates the state derivative
     :param state: current state in vector form at current time step k.
@@ -23,6 +23,6 @@ def integrateRk4(derivative: callable, state: np.array, t: float, dt: float, *ar
     k1 = derivative(t, state, *args)
     k2 = derivative(t + (dt / 2.0), state + dt * (k1 / 2.0), *args)
     k3 = derivative(t + (dt / 2.0), state + dt * (k2 / 2.0), *args)
-    k4 = derivative(t + dt, state + (dt * k3))
+    k4 = derivative(t + dt, state + (dt * k3), *args)
 
     return state + (dt / 6) * (k1 + 2*k2 + 2*k3 + k4)
